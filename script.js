ca3c85efa80ad9fb04945ef2c3c26ef228cc22e7
Graydude98 <rodericksimani@yahoo.com>
2023-08-08 14:45:51 -0400

    started over and entered code for css, js, and index

--- script.js
+++ script.js
@@ -19,9 +19,24 @@ let weather = {
     document.querySelector(".city").innerText = "Weather in " + name;
     document.querySelector(".icon").src = 
         "https://openweathermap.org/img/wn/"+ icon + "@2x.png"
-    document.querySelector(".description").innerText = description
+    document.querySelector(".description").innerText = description;
     document.querySelector(".temp").innerText = temp + "°F";
     document.querySelector(".humidity").innerText = "Humidity:" + humidity +"%";
     document.querySelector(".wind").innerText = "Wind Speed: " + speed + "km/h";
+   },
+   search: function() {
+        this.fetchWeather(document.querySelector(".search-bar").value);
    }
 };
+
+document
+.querySelector(".search button")
+.addEventListener("click", function () {
+    weather.search();
+});
+
+document.querySelector(".search-bar").addEventListener("keyup", function (event) {
+    if (event.key =="Enter") {
+        weather.search();
+    }
+})
